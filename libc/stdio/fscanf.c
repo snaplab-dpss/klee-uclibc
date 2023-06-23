8f8b945dbfc2532e1c6ca67ece9a896f56d1b4d5
Francisco Pereira <fchamicapereira@gmail.com>
2023-06-23 11:40:11 +0100

    patches for symbolic execution

--- libc/stdio/fscanf.c
+++ libc/stdio/fscanf.c
@@ -7,3 +7,10 @@
  */
 #define L_fscanf
 #include "_scanf.c"
+int __isoc99_fscanf(FILE *stream, const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  int ret = vfscanf(stream, format, args);
+  va_end(args);
+  return ret;
+}
