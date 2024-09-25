1777cc54b40c364c75992bb6ae010e3e45dae9e8
Francisco Pereira <fchamicapereira@gmail.com>
2024-09-25 15:52:31 -0400

    Added glibc 2.39 functions

--- libc/stdio/fscanf.c
+++ libc/stdio/fscanf.c
@@ -14,3 +14,11 @@ int __isoc99_fscanf(FILE *stream, const char *format, ...) {
   va_end(args);
   return ret;
 }
+
+int __isoc23_fscanf(FILE *stream, const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  int ret = vfscanf(stream, format, args);
+  va_end(args);
+  return ret;
+}
--- libc/stdio/sscanf.c
+++ libc/stdio/sscanf.c
@@ -14,3 +14,11 @@ int __isoc99_sscanf(const char *str, const char *format, ...) {
   va_end(args);
   return ret;
 }
+
+int __isoc23_sscanf(const char *str, const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  int ret = vsscanf(str, format, args);
+  va_end(args);
+  return ret;
+}
